edcf5f77b9c4d11946e29a8ab9fc412576746f52
Roface <242300108+Roface3@users.noreply.github.com>
2026-01-15 21:55:13 +0100

    feat: added orbital weapon

---
+++ eternal-laughs/67_orb.gd
@@ -0,0 +1,43 @@
+extends Node2D
+# -----------------------------
+# CONFIG
+# -----------------------------
+@export var radius: float = 64.0          # distance from player
+@export var rotation_speed: float = 3.0   # radians per second
+@export var clockwise: bool = true       # spin direction
+
+# -----------------------------
+# NODES
+# -----------------------------
+@onready var player := get_parent()
+@onready var hitbox: Hitbox = get_node("Hitbox") as Hitbox
+@onready var collision: CollisionShape2D = hitbox.get_node("CollisionShape2D") as CollisionShape2D
+
+# -----------------------------
+# RUNTIME STATE
+# -----------------------------
+var angle: float = 0.0  # current angle around player
+
+# -----------------------------
+# READY
+# -----------------------------
+func _ready() -> void:
+	if collision:
+		collision.set_deferred("disabled", false)
+	else:
+		push_error("CollisionShape2D not found under Hitbox!")
+
+# -----------------------------
+# PROCESS
+# -----------------------------
+func _process(delta: float) -> void:
+	if clockwise:
+		angle += rotation_speed * delta
+	else:
+		angle -= rotation_speed * delta
+
+	# Keep angle in 0..2π
+	angle = wrapf(angle, 0, PI * 2)
+
+	# Position orb around player
+	position = Vector2.RIGHT.rotated(angle) * radius
