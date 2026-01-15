a8874d9dae12cbb1ff8fdb853e8fc0b20f81aebb
Roface <242300108+Roface3@users.noreply.github.com>
2026-01-15 20:45:44 +0100

    feat: added endless waves

--- eternal-laughs/scenes/enemies/spawning/enemy_spawner.gd
+++ eternal-laughs/scenes/enemies/spawning/enemy_spawner.gd
@@ -1,51 +1,77 @@
 extends Node2D
 
-@export var spawns: Array[Spawn_info] = []
+@export var enemy_type_1: PackedScene # weakest
+@export var enemy_type_2: PackedScene # medium
+@export var enemy_type_3: PackedScene # strongest
+
+@export var max_enemies := 150
+@export var difficulty_step_time := 10 # seconds per difficulty increase
+
 @onready var player = get_tree().get_first_node_in_group("player")
-var time = 0
+
+var time := 0
+var difficulty := 1
+var spawn_timer := 0
 
 func _on_timer_timeout() -> void:
 	time += 1
-	var enemy_spawns = spawns
-	
-	for i in enemy_spawns:
-		if time >= i.time_start and time <= i.time_end:
-			if i.spawn_delay_counter < i.enemy_spawn_delay:
-				i.spawn_delay_counter += 1
-			else:
-				i.spawn_delay_counter = 0
-				var new_enemy = load(str(i.enemy.resource_path))
-				var counter = 0
-				while counter < i.enemy_num:
-					var enemy_spawn = new_enemy.instantiate()
-					enemy_spawn.global_position = get_random_position()
-					add_child(enemy_spawn)
-					counter += 1
+	difficulty = int(time / difficulty_step_time) + 1
+	spawn_enemies()
 
-func get_random_position():
-	var viewport_rect = get_viewport_rect().size * randf_range(1.1,1.4)
-	var top_left = Vector2(player.global_position.x - viewport_rect.x/2,player.global_position.y - viewport_rect.y/2)
-	var top_right = Vector2(player.global_position.x + viewport_rect.x/2,player.global_position.y - viewport_rect.y/2)
-	var bottom_left = Vector2(player.global_position.x - viewport_rect.x/2,player.global_position.y + viewport_rect.y/2)
-	var bottom_right = Vector2(player.global_position.x + viewport_rect.x/2,player.global_position.y + viewport_rect.y/2)
-	
-	var pos_side = ["up","down","right","left"].pick_random()
-	var spawn_pos1 = Vector2.ZERO
-	var spawn_pos2 = Vector2.ZERO
-	
-	match pos_side:
+func spawn_enemies() -> void:
+	if not can_spawn():
+		return
+	if spawn_timer > 0:
+		spawn_timer -= 1
+		return
+	spawn_timer = get_spawn_delay()
+	for i in get_spawn_count():
+		var enemy_scene := get_enemy()
+		var enemy := enemy_scene.instantiate()
+		enemy.global_position = get_random_position()
+		add_child(enemy)
+
+func get_spawn_count() -> int:
+	return clamp(1 + difficulty / 2, 1, 15)
+
+func get_spawn_delay() -> int:
+	return max(1, 6 - difficulty / 3)
+
+func get_enemy() -> PackedScene:
+	# Time-based unlocks (VS style)
+	if time < 120:
+		return enemy_type_1
+	elif time < 300:
+		return [enemy_type_1, enemy_type_2].pick_random()
+	else:
+		return [enemy_type_1, enemy_type_2, enemy_type_3].pick_random()
+
+func can_spawn() -> bool:
+	return get_tree().get_nodes_in_group("enemy").size() < max_enemies
+
+func get_random_position() -> Vector2:
+	var viewport_size: Vector2 = get_viewport_rect().size * randf_range(1.1, 1.4)
+	var top_left: Vector2 = player.global_position - viewport_size / 2.0
+	var bottom_right: Vector2 = player.global_position + viewport_size / 2.0
+	match ["up", "down", "left", "right"].pick_random():
 		"up":
-			spawn_pos1 = top_left
-			spawn_pos2 = top_right
+			return Vector2(
+				randf_range(top_left.x, bottom_right.x),
+				top_left.y
+			)
 		"down":
-			spawn_pos1 = bottom_left
-			spawn_pos2 = bottom_right
-		"right":
-			spawn_pos1 = top_right
-			spawn_pos2 = bottom_right
+			return Vector2(
+				randf_range(top_left.x, bottom_right.x),
+				bottom_right.y
+			)
 		"left":
-			spawn_pos1 = top_left
-			spawn_pos2 = bottom_left
-	var x_spawn = randf_range(spawn_pos1.x,spawn_pos2.x)
-	var y_spawn = randf_range(spawn_pos1.y,spawn_pos2.y)
-	return Vector2(x_spawn,y_spawn)
+			return Vector2(
+				top_left.x,
+				randf_range(top_left.y, bottom_right.y)
+			)
+		"right":
+			return Vector2(
+				bottom_right.x,
+				randf_range(top_left.y, bottom_right.y)
+			)
+	return player.global_position
